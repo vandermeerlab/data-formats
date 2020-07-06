86b0c2390420d7a4def2ae453c16bfdbc56a2ca5
transedward <transedward@gmail.com>
2020-07-06 16:32:44 -0700

    Add unit ids from mclust loader

---
+++ loader_mclust.py
@@ -0,0 +1,81 @@
+# Code ported from nept: https://github.com/vandermeerlab/nept/blob/master/nept/loaders_mclust.py
+import os
+import numpy as np
+from spiketrain import SpikeTrain
+
+def load_mclust_t(filename):
+    """Loads a mclust .t tetrode file.
+    Parameters
+    ----------
+    filename: str
+    Returns
+    -------
+    times: np.array
+    """
+    # The format for a .t file according the the mclust docs is
+    # header - beginning with %%BEGINHEADER and ending with %%ENDHEADER
+    # uint64 - timestamp in tenths of ms (big endian)
+    with open(filename, 'rb') as f:
+        file_contents = f.read()
+
+    # Here we separate the mclust header from the timestamps (data).
+    header_begin_idx = file_contents.find(b'%%BEGINHEADER')
+    header_end_idx = file_contents.find(b'%%ENDHEADER') + len(b'%%ENDHEADER\n')
+    header = file_contents[header_begin_idx:header_end_idx]
+
+    data = file_contents[header_end_idx:]
+    spike_times = np.fromstring(data, dtype=np.dtype('uint32').newbyteorder('>'))
+
+    # Since some .t files are in uint32 and others are in uint64,
+    # we can load all as uint32 (L) but have to remove all the 0's
+    # that result from interpreting a uint64 as a uint32.
+    spike_times = spike_times[spike_times > 0]
+
+    # Spikes times are in timestamps (tenths of ms).
+    # Let's convert the timestamps to seconds.
+    spikes = spike_times / 1e4
+
+    return spikes
+
+
+def get_spiketrain(spike_times, label):
+    """Converts spike times to nept.SpikeTrain.
+    Parameters
+    ----------
+    spike_times: np.array
+    label: str
+    Returns
+    -------
+    spiketrain: SpikeTrain
+    """
+
+    return SpikeTrain(spike_times, label)
+
+
+def load_spikes(filepath, load_questionable=True):
+    """Loads spikes from multiple tetrode spike files from a given session.
+    Parameters
+    ----------
+    filepath: str
+        Session folder
+    load_questionable: boolean
+        Loads ``*.t`` and ``*._t`` spiketrains if True (default).
+    Returns
+    -------
+    spikes: list of nept.SpikeTrain
+    """
+    spikes = []
+
+    for file in os.listdir(filepath):
+        if file.endswith(".t"):
+            label = file[18:-2]
+            spiketrain = get_spiketrain(load_mclust_t(os.path.join(filepath, file)), label)
+            spikes.append(spiketrain)
+
+        if load_questionable:
+            if file.endswith("._t"):
+                label = file[18:-2]
+                spiketrain = get_spiketrain(load_mclust_t(os.path.join(filepath, file)), label)
+                spikes.append(spiketrain)
+
+    return np.array(spikes)
---
+++ spiketrain.py
@@ -0,0 +1,74 @@
+# Code ported from nept: https://github.com/vandermeerlab/nept/blob/master/nept/core/spiketrain.py
+import numpy as np
+
+class SpikeTrain:
+    """A set of spike times associated with an individual putative neuron.
+    Parameters
+    ----------
+    time : np.array
+    label : str or None, optional
+        Information pertaining to the source of the spiketrain.
+    Attributes
+    ----------
+    time : np.array
+        With shape (n_samples,).
+    label : str or None
+        Information pertaining to the source of the spiketrain.
+    """
+    def __init__(self, time, label=None):
+        time = np.squeeze(time).astype(float)
+
+        if time.shape == ():
+            time = time[..., np.newaxis]
+
+        if time.ndim != 1:
+            raise ValueError("time must be a vector")
+
+        if label is not None and not isinstance(label, str):
+            raise ValueError("label must be a string")
+
+        self.time = np.sort(time)
+        self.label = label
+
+    def __getitem__(self, idx):
+        return SpikeTrain(self.time[idx], self.label)
+
+    @property
+    def n_spikes(self):
+        """(int) Number of samples."""
+        return self.time.size
+
+    def time_slice(self, t_starts, t_stops):
+        """Creates a new object corresponding to the time slice of
+        the original between (and including) times t_start and t_stop. Setting
+        either parameter to None uses infinite endpoints for the time interval.
+        Parameters
+        ----------
+        spiketrain : nept.SpikeTrain
+        t_starts : float or list or None
+        t_stops : float or list or None
+        Returns
+        -------
+        sliced_spiketrain : nept.SpikeTrain
+        """
+        if isinstance(t_starts, (int, float)) or t_starts is None:
+            t_starts = [t_starts]
+
+        if any(element is None for element in t_starts):
+            t_starts = [min(self.time) if t_start is None else t_start for t_start in t_starts]
+
+        if isinstance(t_stops, (int, float)) or t_stops is None:
+            t_stops = [t_stops]
+
+        if any(element is None for element in t_stops):
+            t_stops = [max(self.time) if t_start is None else t_start for t_start in t_stops]
+
+        if len(t_starts) != len(t_stops):
+            raise ValueError("must have same number of start and stop times")
+
+        indices = []
+        for t_start, t_stop in zip(t_starts, t_stops):
+            indices.append((self.time >= t_start) & (self.time <= t_stop))
+        indices = np.any(np.column_stack(indices), axis=1)
+
+        return self[indices]
